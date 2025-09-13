0a3eb157f1395152a127ddff3b7ff4f79fe41450
yh_21 <yh_21@naver.com>
2025-09-13 16:07:41 +0900

    modify selection_sort.py file

--- week_2/selection_sort.py
+++ week_2/selection_sort.py
@@ -12,13 +12,26 @@
 num = [5, 4, 1, 8, 7, 2, 6, 3]
 
 for i in range(0, len(num)):
-    current = num[i]
-    min_value = current
+    min_value = num[i]
     for j in range(i + 1, len(num)):
         if num[j] < min_value:
             min_value = num[j]
             count = j
     if min_value != num[i]:
         num[count], num[i] = num[i], num[count]
+
+print(num)
+
+# count가 정의 안될 수도 있음
+# min_idx가 i인 경우 자체 swap도 가능
+
+num = [5, 4, 1, 8, 7, 2, 6, 3]
+
+for i in range(len(num)):
+    min_idx = i
+    for j in range(i + 1, len(num)):
+        if num[j] < num[min_idx]:
+            min_idx = j
+    num[i], num[min_idx] = num[min_idx], num[i]
 
 print(num)
