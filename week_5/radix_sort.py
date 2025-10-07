c0f956957101ae045fe8efb08c9b98c92c738c99
yh_21 <yh_21@naver.com>
2025-10-08 00:59:50 +0900

    add radix_sort.py file

---
+++ week_5/radix_sort.py
@@ -0,0 +1,20 @@
+nums = [329, 457, 657, 839, 436, 720, 355]
+dig = int(len(str(max(nums))))
+# print(M)
+
+def digit(n, d):
+    if n >= 10 ** (d - 1):
+        return int(str(n)[-d])
+    else:
+        return 0
+    
+for i in range(1, dig + 1):
+    bucket = [[] for _ in range(10)]
+    for number in nums:
+        bucket[digit(number, i)].append(number)
+    nums = list()
+    for j in range(10):
+        for number in bucket[j]:
+            nums.append(number)
+    
+print(nums)
